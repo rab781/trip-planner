90a0b7dae4c63d06b52a85fb8d5504963f39c156
rab781 <raihanrabbani199@gmail.com>
2026-01-10 16:03:10 +0700

    first commit

---
+++ database/migrations/2026_01_10_085358_create_transport_rates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transport_rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('transport_type');
+            $table->decimal('base_fare', 8, 2);
+            $table->decimal('rate_per_km', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transport_rates');
+    }
+};
---
+++ database/migrations/2026_01_10_085918_create_destinations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->id('zone_id');
+            $table->id('category_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_url')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->decimal('rating', 3, 2)->default(0);
+            $table->time('best_visit_time')->nullable();
+            $table->time('opening_time')->nullable();
+            $table->time('closing_time')->nullable();
+            $table->integer('avg_visit_duration_minutes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
